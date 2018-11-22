d7249e1e4db353adb1d52f3c97bffbb6f00e04ce
EvgenBES <40202338+EvgenBES@users.noreply.github.com>
2018-11-22 17:11:32 +0300

    min fix

--- app/src/main/java/com/example/fox/mrtestproject/view/screeens/MainPresenter.java
+++ app/src/main/java/com/example/fox/mrtestproject/view/screeens/MainPresenter.java
@@ -1,5 +1,7 @@
 package com.example.fox.mrtestproject.view.screeens;
 
+import android.util.Log;
+
 import com.example.fox.mrtestproject.app.App;
 import com.example.fox.mrtestproject.entity.User;
 import com.example.fox.mrtestproject.usecases.AddUserUseCase;
@@ -10,12 +12,15 @@ import com.example.fox.mrtestproject.usecases.GetSortTimeUserUseCase;
 import com.example.fox.mrtestproject.usecases.SearchUserUseCase;
 import com.example.fox.mrtestproject.view.base.BaseViewModel;
 
+import org.reactivestreams.Subscription;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import io.reactivex.functions.Consumer;
+import io.reactivex.internal.fuseable.ConditionalSubscriber;
 
 public class MainPresenter extends BaseViewModel {
 
@@ -49,15 +54,7 @@ public class MainPresenter extends BaseViewModel {
             addUserUseCase
                     .addUser(addUsers());
 
-            getAllUserUseCase
-                    .getListUser()
-                    .subscribe(new Consumer<List<User>>() {
-                        @Override
-                        public void accept(List<User> users) throws Exception {
-                            mainView.setItems(users);
-                        }
-                    })
-                    .isDisposed();
+            loadUserDB();
         }
     }
 
@@ -76,36 +73,74 @@ public class MainPresenter extends BaseViewModel {
         listUsers.add(new User(4, "Balan Dmitriy Bedrosovich", "8А", 325, 15));
         listUsers.add(new User(5, "Grigor Stepan Iosevech", "6В", 96, 44));
 
-
         return listUsers;
     }
 
 
+    public void loadUserDB() {
+        getAllUserUseCase
+                .getListUser()
+                .subscribe(new Consumer<List<User>>() {
+                    @Override
+                    public void accept(List<User> users) throws Exception {
+                        try {
+                            mainView.setItems(users);
+                        } catch (Exception ignored) {
+
+                        }
+
+                    }
+                })
+                .isDisposed();
+
+    }
+
     public void sortNameUser() {
         sortNameUserUseCase
                 .getSortNameUser()
-                .subscribe(users -> mainView.setItems(users))
+                .subscribe(users -> {
+                    try {
+                        mainView.setItems(users);
+                    } catch (Exception ignored) {
+                    }
+                })
                 .isDisposed();
     }
 
     public void sortTimeUser() {
         sortTimeUserUseCase
                 .getSortTimeUser()
-                .subscribe(users -> mainView.setItems(users))
+                .subscribe(users -> {
+                    try {
+                        mainView.setItems(users);
+                    } catch (Exception ignored) {
+                    }
+                })
                 .isDisposed();
     }
 
     public void sortPointUser() {
         sortPointUserUseCase
                 .getSortPointUser()
-                .subscribe(users -> mainView.setItems(users))
+                .subscribe(users -> {
+                    try {
+                        mainView.setItems(users);
+                    } catch (Exception ignored) {
+
+                    }
+                })
                 .isDisposed();
     }
 
     public void searchUser(String search) {
         searchUserUseCase
                 .searchUser(search)
-                .subscribe(users -> mainView.setItems(users))
+                .subscribe(users -> {
+                    try {
+                        mainView.setItems(users);
+                    } catch (Exception ignored) {
+                    }
+                })
                 .isDisposed();
     }
 }
